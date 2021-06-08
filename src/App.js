10a2f20d2bdda1aad0781de37883ca25914dc13a
Anton <anton@Antons-MacBook-Pro-2.local>
2021-06-08 21:24:01 +0400

    initial logic added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [auth, setAuth] = useState("");
+  const [applicationId, setApplicationId] = useState("");
+  const [environmentName, setEnvironmentName] = useState("");
+  const [allFlags, setAllFlags] = useState([]);
+
+  const handleShowAll = () => {
+    fetch(
+      `https://x-api.rollout.io/public-api/applications/${applicationId}/${environmentName}/flags`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          authorization: auth,
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+      setAllFlags(res.data);
+    });
+  };
+
+  const handleChange = (event, type) => {
+    switch (type) {
+      case "auth":
+        return setAuth(event.target.value);
+      case "appId":
+        return setApplicationId(event.target.value);
+      case "envName":
+        return setEnvironmentName(event.target.value);
+      default:
+        return;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rox flags management dashboard</h1>
+      <div className={"getFlags"}>
+        <label>
+          <span>authorization</span>
+          <input
+            type="text"
+            name={"auth"}
+            onChange={(event) => handleChange(event, "auth")}
+          />
+        </label>
+        <label>
+          <span>applicationId</span>
+          <input
+            type="text"
+            name={"appId"}
+            onChange={(event) => handleChange(event, "appId")}
+          />
+        </label>
+        <label>
+          <span>environmentName</span>
+          <input
+            type="text"
+            name={"envName"}
+            onChange={(event) => handleChange(event, "envName")}
+          />
+        </label>
+
+        <button onClick={handleShowAll}>show all flags</button>
+
+        {allFlags.length && allFlags.map((flag) => <div>{flag.name}</div>)}
+      </div>
     </div>
   );
 }
