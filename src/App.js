a9f500f1631f77aca340cb61943ac93227477910
Ardmort <anton.borisov@optimax.dev>
2021-06-09 10:40:42 +0400

    added remove logic

--- src/App.js
+++ src/App.js
@@ -19,9 +19,37 @@ function App() {
       }
     ).then((res) => {
       console.log(res);
-      setAllFlags(res.data);
+      setAllFlags(res);
     });
   };
+
+  const handleRemoveNonOriginals = () => {
+    let nonOriginals = allFlags.filter(flag => flag.name.includes('.'))
+
+    nonOriginals.forEach(flag => {
+      fetch(`https://x-api.rollout.io/public-api/applications/${applicationId}/flags/${flag.name}`, {
+        method: "DELETE",
+        headers: {
+          authorization: auth,
+        },
+      }).then(() => {
+        console.log(`flag ${flag.name} removed`)})
+    })
+
+    fetch(
+        `https://x-api.rollout.io/public-api/applications/${applicationId}/${environmentName}/flags`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            authorization: auth,
+          },
+        }
+    ).then((res) => {
+      setAllFlags(res);
+    });
+
+  }
 
   const handleChange = (event, type) => {
     switch (type) {
@@ -41,33 +69,36 @@ function App() {
       <h1>Rox flags management dashboard</h1>
       <div className={"getFlags"}>
         <label>
-          <span>authorization</span>
           <input
             type="text"
             name={"auth"}
             onChange={(event) => handleChange(event, "auth")}
           />
+          <span>authorization</span>
         </label>
         <label>
-          <span>applicationId</span>
           <input
             type="text"
             name={"appId"}
             onChange={(event) => handleChange(event, "appId")}
           />
+          <span>applicationId</span>
         </label>
         <label>
-          <span>environmentName</span>
           <input
             type="text"
             name={"envName"}
             onChange={(event) => handleChange(event, "envName")}
           />
+          <span>environmentName</span>
         </label>
 
-        <button onClick={handleShowAll}>show all flags</button>
+        <div><button onClick={handleShowAll} className={'showAllButton'}>show all flags</button></div>
 
-        {allFlags.length && allFlags.map((flag) => <div>{flag.name}</div>)}
+        {allFlags.length ? allFlags.map((flag) => <div>{flag.name}</div>) : null}
+        <div className={'remove'}>
+          <button onClick={handleRemoveNonOriginals}>remove all flags with non-blank namespace</button>
+        </div>
       </div>
     </div>
   );
